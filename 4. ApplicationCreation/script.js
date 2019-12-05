be854ec25265f17fe0ba5249bf7444ac19ebe664
Kungurov Roman <vakungur@gmail.com>
2019-12-05 13:41:44 +0300

    Практика. Создание приложения. Случайные значения

--- 4. ApplicationCreation/script.js
+++ 4. ApplicationCreation/script.js
@@ -24,14 +24,24 @@ function handlBoxClick(event) {
 function renderBox() {
     $game.innerHTML = ''
     var box = document.createElement('div')
+    var boxSize = getRandom(30, 100)
+    var gameSize = $game.getBoundingClientRect()
+    var maxTop = gameSize.height - boxSize
+    var maxLeft = gameSize.width - boxSize
+    console.log(gameSize)
 
-    box.style.height = box.style.width = '50px'
+
+    box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '50px'
-    box.style.left = '70px'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-box', true)
 
     $game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
 }
